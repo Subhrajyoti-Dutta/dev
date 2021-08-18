319bfa405f75f7df44a7ac013520b4b5e56a44cc
Subhrajyoti <duttajyotisubhra@gmail.com>
2021-08-19 02:24:17 +0530

    daily update

---
+++ python/rough2.py
@@ -0,0 +1,68 @@
+a = """lakshya bhardwaj
+Gaurav Kumar Yadav
+Ishaan Dhull
+ASWIN G
+SNEHA BHALLA
+Amit kumar
+Rituparna Das
+Sanskar
+Vaishnav srivastava
+Hari Krishna Dhamodaran
+Nijesh S P
+Neeraj Kumar Singh
+KAUSHLENDRA YADAV
+S.YUVARAJAN
+Sanjana Komateswar
+Kirti Gupta
+Girivasan R
+Siddharth Shah
+Shubham sharma 
+Dadasaheb Salunke
+Akhil Pratap Singh
+T Manauv
+Himanshu Tripathi
+Bapyndap Ryndem 
+Sebin Bejoy 
+Mufeed
+Jithesh Gopal
+Sarbeswar Das
+Harshavardhan Suresh
+Chandra Raveendranath
+Harish B
+Syed Ali Mujtaba
+Rahul M
+Harshit Dev Yadav
+Triparna Bagish
+lokesh jadhav.
+Chikirsha Gulati
+Kavali krishna chaitanya
+ANJANA SHREE R J
+PRANAM KRISHNA BHARDWAJ
+Anand Hari Natarajan
+PUNIT SINGH
+Shivam Srivastava
+deepak 
+Abishek Sajeev
+Gourav Saggu
+Ansh Bhardwaj
+Binoy Pappachan 
+Ashutosh Chandra Pant
+B GOKUL KUMAR
+Abdul Rahuman M
+Kayalvizhi R
+ARSALAN AHMAD WARSI
+Shaurya Malhan
+Vasu Garg
+Varun Raja
+Aparup Das
+Prasanna Shastri
+Dilbacker MM
+Gouri Agrawal
+Neethu Lal
+Chandan Kumar
+Aarya Abhay Pingle
+Akshaya R
+Kartikey Singh
+Shreya Shambhavi """.split("\n")
+
+print(a)
