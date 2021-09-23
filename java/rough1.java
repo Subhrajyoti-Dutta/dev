cfb503d51d2349ec9a0e0457f76187e67a0609ab
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2021-09-23 19:08:09 +0530

    Daily Update

--- java/rough1.java
+++ java/rough1.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class rough1{
     public static void main(String[] args){
-        /*Start Your Code Here*/
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
     }
 }
