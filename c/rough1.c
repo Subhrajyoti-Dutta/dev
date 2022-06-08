6284ea199c9241d0c1b88b70e3b4fc3680af6f47
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2022-06-08 13:27:25 +0530

    Regular update

--- c/rough1.c
+++ c/rough1.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
-int func(int x, int y) {
-    return x + y;
-}
 
 int main() {
-    int a, b;
-    a = 12;
-    b = 7;
-    printf("%d", func(a, b));
+    int a[3][3];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        for (int j = i + 1; j < 3; j++) {
+            // printf("%d %d %d %d %d %d \n", a[0][i] * a[1][j], a[0][i] * a[1][j], a[0][i] * a[2][j], a[2][i] * a[0][j], a[1][i] * a[2][j], a[2][i] * a[1][j]);
+            if (a[0][i] * a[1][j] == a[1][i] * a[0][j] && a[0][i] * a[2][j] == a[2][i] * a[0][j] && a[1][i] * a[2][j] == a[2][i] * a[1][j]) {
+                printf("%d#%d", i, j);
+                return 0;
+            }
+        }
+
+    }
+    printf("0#0");
     return 0;
 }
--- python/rough1.py
+++ python/rough1.py
@@ -1,42 +1,7 @@
-def tuplesort(L,ind):
-	L_ = [i for i in L]
-	L_.sort(key=lambda arr : arr[ind])
-	return L_
+import numpy as np
 
-a = ((0,2,2),(1,3,7),(2,5,6),(3,1,5),(4,10,1))
+print(np.add(np.array([1]), np.array([])))  # outputs empty array
 
-# print(tuplesort(a,2))
+# print(np.add(np.array([1, 2]), np.array([])))  # raises ValueError
 
-def vol(arr):
-	# print(arr)
-	return 3.14 * (arr[1]/2)**2 * arr[2]
-
-def tallTower(L):
-	L_ = list(L)
-	L_.sort(key=lambda arr : arr[1],reverse=True)
-	pre = L_[0]
-	print(L_)
-	# print([vol(i) for i in L_])
-	i=1
-	while i < len(L_):
-		print(i)
-		print(L_)
-		print([vol(i) for i in L_])
-		if (vol(L_[i]) >= vol(pre)/2):
-			print("if")
-			L_.pop(i)
-		elif(L_[i][1] > pre[1]):
-			print("elif")
-			L_.pop(i)
-		else:
-			print("else")
-			pre = L_[i]
-			i += 1
-	height = 0
-	print(L_)
-	print([vol(i) for i in L_])
-	for i in L_:
-		height+=i[2]
-	return height
-
-print(tallTower(a))
+print(np.source(np.add))
